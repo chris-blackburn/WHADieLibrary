f7d61daa2a6cd49f9b9d6f91be150291a3e7ca8f
krizboy12 <krizboy12@gmail.com>
2017-07-20 10:35:06 -0400

    Accidently pointed the new die input to the die table instead of the job table

--- dielibrary.com/php/newDieForm.php
+++ dielibrary.com/php/newDieForm.php
@@ -5,7 +5,7 @@
 		<input type="hidden" name="dieFunction" value="add">
 		<input type="hidden" name="jobFunction" value="add">
 
-		<input type="hidden" name="!newDie" value="yes">
+		<input type="hidden" name="?newDie" value="yes">
 
 		<div class="field center">
 			<input type="number" name="?jobNumber" placeholder="Job number"  min="0" required>
