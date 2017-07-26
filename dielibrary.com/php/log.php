63ca4dcdc88093b78f923ab3ca694bb6a0505146
krizboy12 <krizboy12@gmail.com>
2017-07-26 13:01:40 -0400

    forgot to add the log.php file

---
+++ dielibrary.com/php/log.php
@@ -0,0 +1,24 @@
+<?php
+
+	function brackets($str) {
+		return "[ " . $str . " ]";
+	}
+
+	// grab the log data
+	$data = $_POST['log'];
+
+	// grab the date and time for entry in the log file
+	$date = Date("m-d-Y H:i:s");
+
+	$log = brackets($_SERVER['REMOTE_ADDR'] . ":" . $_SERVER['SERVER_PORT']) . " " . brackets($date) . " " . $data;
+
+	// write to the file
+	$file = "../logs/client_logs.clogs";
+
+	if (file_put_contents($file, $log, FILE_APPEND | LOCK_EX)) {
+		// check file size and limit logs
+	} else {
+		die(error_get_last()["message"]);
+	}
+
+?>
