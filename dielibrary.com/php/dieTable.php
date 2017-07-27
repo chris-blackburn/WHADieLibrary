6ed6f1e0ddf210bd5408524ec28280f686f13776
krizboy12 <krizboy12@gmail.com>
2017-07-27 17:04:48 -0400

    row count for tables

--- dielibrary.com/php/dieTable.php
+++ dielibrary.com/php/dieTable.php
@@ -13,4 +13,26 @@
 	<tbody class="table-body">
 		<!-- This gets populated by populateTable.php, see js/jquery.js -->
 	</tbody>
+
+	<span><b>Number of Records: </b>
+		<?php
+			require_once "Database.php";
+
+			ob_start();
+			$db = new Database(DB_HOST, DB_DBASE);
+			$db->connect(DB_USER, DB_PASS);
+
+			$sql = "SELECT count(*) FROM " . DIE_TABLE;
+
+			$result = $db->query($sql);
+			ob_end_clean();
+
+			echo $result->fetch_array()[0];
+
+			ob_start();
+			$result->free_result();
+			$db->disconnect();
+			ob_end_clean();
+		?>
+	</span>
 </table>
